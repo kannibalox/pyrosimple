61d3bc8c8dc7a0cb97f1c9630fa44a79d889b17d
kannibalox <kannibalox@gmail.com>
2021-10-17 13:35:08 -0400

    Formatting

--- src/pyrocore/scripts/lstor.py
+++ src/pyrocore/scripts/lstor.py
@@ -31,36 +31,39 @@ from pyrocore.util import fmt, metafile
 
 
 class MetafileLister(ScriptBase):
-    """ List contents of a bittorrent metafile.
-    """
+    """List contents of a bittorrent metafile."""
 
     # argument description for the usage information
     ARGS_HELP = "<metafile>..."
 
-
     def add_options(self):
-        """ Add program options.
-        """
-        self.add_bool_option("--reveal",
-            help="show full announce URL including keys")
-        self.add_bool_option("--raw",
-            help="print the metafile's raw content in all detail")
-        self.add_bool_option("-V", "--skip-validation",
-            help="show broken metafiles with an invalid structure")
-        self.add_value_option("-o", "--output", "KEY,KEY1.KEY2,...",
-            action="append", default=[],
+        """Add program options."""
+        self.add_bool_option("--reveal", help="show full announce URL including keys")
+        self.add_bool_option(
+            "--raw", help="print the metafile's raw content in all detail"
+        )
+        self.add_bool_option(
+            "-V",
+            "--skip-validation",
+            help="show broken metafiles with an invalid structure",
+        )
+        self.add_value_option(
+            "-o",
+            "--output",
+            "KEY,KEY1.KEY2,...",
+            action="append",
+            default=[],
             help="select fields to print, output is separated by TABs;"
-                 " note that __file__ is the path to the metafile,"
-                 " __hash__ is the info hash,"
-                 " and __size__ is the data size in bytes"
-         )
+            " note that __file__ is the path to the metafile,"
+            " __hash__ is the info hash,"
+            " and __size__ is the data size in bytes",
+        )
         # TODO: implement this
-        #self.add_value_option("-c", "--check-data", "PATH",
+        # self.add_value_option("-c", "--check-data", "PATH",
         #    help="check the hash against the data in the given path")
 
     def mainloop(self):
-        """ The main loop.
-        """
+        """The main loop."""
         if not self.args:
             self.parser.print_help()
             self.parser.exit()
@@ -74,12 +77,22 @@ class MetafileLister(ScriptBase):
             try:
                 # Read and check metafile
                 try:
-                    data = metafile.checked_open(filename, log=self.LOG if self.options.skip_validation else None,
-                        quiet=(self.options.quiet and (self.options.output or self.options.raw)))
+                    data = metafile.checked_open(
+                        filename,
+                        log=self.LOG if self.options.skip_validation else None,
+                        quiet=(
+                            self.options.quiet
+                            and (self.options.output or self.options.raw)
+                        ),
+                    )
                 except EnvironmentError as exc:
-                    self.fatal("Can't read '%s' (%s)" % (
-                        filename, str(exc).replace(": '%s'" % filename, ""),
-                    ))
+                    self.fatal(
+                        "Can't read '%s' (%s)"
+                        % (
+                            filename,
+                            str(exc).replace(": '%s'" % filename, ""),
+                        )
+                    )
                     raise
 
                 listing = None
@@ -88,50 +101,57 @@ class MetafileLister(ScriptBase):
                     if not self.options.reveal and "info" in data:
                         # Shorten useless binary piece hashes
                         data["info"]["pieces"] = "<%d piece hashes>" % (
-                            len(data["info"]["pieces"]) / len(hashlib.sha1().digest()) # bogus pylint: disable=E1101
+                            len(data["info"]["pieces"])
+                            / len(
+                                hashlib.sha1().digest()
+                            )  # bogus pylint: disable=E1101
                         )
                     else:
                         data["info"]["pieces"] = data["info"]["pieces"].decode("latin1")
 
                     listing = json.dumps(data, indent=2)
                 elif self.options.output:
+
                     def splitter(fields):
                         "Yield single names for a list of comma-separated strings."
                         for flist in fields:
-                            for field in flist.split(','):
+                            for field in flist.split(","):
                                 yield field.strip()
 
                     data["__file__"] = filename
-                    if 'info' in data:
+                    if "info" in data:
                         data["__hash__"] = metafile.info_hash(data)
                         data["__size__"] = metafile.data_size(data)
                     values = []
                     for field in splitter(self.options.output):
                         try:
                             val = data
-                            for key in field.split('.'):
+                            for key in field.split("."):
                                 val = val[key]
                         except KeyError as exc:
-                            self.LOG.error("%s: Field %r not found (%s)" % (filename, field, exc))
+                            self.LOG.error(
+                                "%s: Field %r not found (%s)" % (filename, field, exc)
+                            )
                             break
                         else:
                             values.append(str(val))
                     else:
-                        listing = '\t'.join(values)
+                        listing = "\t".join(values)
                 else:
-                    listing = '\n'.join(torrent.listing(masked=not self.options.reveal))
+                    listing = "\n".join(torrent.listing(masked=not self.options.reveal))
             except (ValueError, KeyError, bencode.BencodeDecodeError) as exc:
                 if self.options.debug:
                     raise
-                self.LOG.warning("Bad metafile %r (%s: %s)" % (filename, type(exc).__name__, exc))
+                self.LOG.warning(
+                    "Bad metafile %r (%s: %s)" % (filename, type(exc).__name__, exc)
+                )
             else:
                 if listing is not None:
                     print(listing)
 
 
-def run(): #pragma: no cover
-    """ The entry point.
-    """
+def run():  # pragma: no cover
+    """The entry point."""
     ScriptBase.setup()
     MetafileLister().run()
 
