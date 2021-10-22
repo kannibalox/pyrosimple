ce3ffd3f4337233f51684bbb0bf782acb3d0d9ad
kannibalox <kannibalox@gmail.com>
2021-10-21 21:09:01 -0400

    Lint fixes

--- src/pyrosimple/torrent/jobs.py
+++ src/pyrosimple/torrent/jobs.py
@@ -18,10 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-try:
-    import json
-except ImportError:
-    import simplejson as json # pylint: disable=F0401
+import json
 
 import requests
 from requests.exceptions import RequestException
