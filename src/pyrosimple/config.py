de6f21a8590f953314db680b8d8e607a1a904c44
kannibalox <kannibalox@gmail.com>
2022-05-01 13:39:37 -0400

    Typing fixes

--- src/pyrosimple/config.py
+++ src/pyrosimple/config.py
@@ -24,7 +24,7 @@ import logging
 import urllib
 
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
 from dynaconf import Dynaconf, Validator
 
@@ -52,13 +52,14 @@ settings = Dynaconf(
     ],
 )
 
-def scgi_url_from_rtorrentrc(rcfile: str) -> Optional[str]:
+def scgi_url_from_rtorrentrc(rcfile: Union[str,Path]) -> Optional[str]:
     """Parse a rtorrent.rc file and """
     log = logging.getLogger(__name__)
     log.debug("Loading rtorrent config from '%s'", rcfile)
     scgi_local: str = ""
     scgi_port: str = ""
-    with open(rcfile, "r", encoding="utf-8") as handle:
+    rcfile = Path(rcfile)
+    with rcfile.open("r", encoding="utf-8") as handle:
         continued = False
         for line in handle.readlines():
             # Skip comments, continuations, and empty lines
