21a11649794d06169a858c3f24e2f1da3a88eac8
kannibalox <kannibalox@gmail.com>
2022-06-12 09:44:21 -0400

    Remove relative value from tests

--- src/tests/test_matching.py
+++ src/tests/test_matching.py
@@ -103,7 +103,6 @@ def test_parsim_error_conditions(cond):
     ("cond", "expected"),
     [
         ("name=arch", '"string.contains_i=$d.name=,\\"arch\\""'),
-        ("name=arch-* OR [ alias=Ubuntu loaded>1w ]", "")
     ],
 )
 def test_conditions_prefilter(cond, expected):
