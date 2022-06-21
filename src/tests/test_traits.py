0bc7f50bbfdc92f8ce3f9776baabccb2e36bfa00
kannibalox <kannibalox@gmail.com>
2022-06-21 12:13:07 -0400

    Add minor trait testing

--- src/tests/test_traits.py
+++ src/tests/test_traits.py
@@ -21,4 +21,16 @@
 import logging
 import unittest
 
+import pytest
+
 from pyrosimple.util import traits
+
+@pytest.mark.parametrize(
+    ('name', "alias", "filetype", "result"),
+    [
+        ('Test', None, None, []),
+        ('Test.tgz', None, '.tgz', ['misc', 'tgz'])
+    ]
+)
+def test_trait_detect(name, alias, filetype, result):
+    assert traits.detect_traits(name, alias, filetype) == result
