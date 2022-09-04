577ede450ea256a210a9a00d5735036513a67b85
kannibalox <kannibalox@gmail.com>
2022-09-04 12:48:10 -0400

    Add backfill command

--- src/pyrosimple/scripts/pyroadmin.py
+++ src/pyrosimple/scripts/pyroadmin.py
@@ -3,10 +3,13 @@
     Copyright (c) 2010 The PyroScope Project <pyroscope.project@gmail.com>
 """
 
+from datetime import datetime
+from pathlib import Path
 
 import pyrosimple
 
 from pyrosimple import config
+from pyrosimple.util import matching
 from pyrosimple.scripts.base import ScriptBase, ScriptBaseWithConfig
 
 
@@ -20,11 +23,54 @@ class AdminTool(ScriptBaseWithConfig):
         super().add_options()
         self.parser.set_defaults(func=None)
         subparsers = self.parser.add_subparsers()
-        config_parser = subparsers.add_parser("config")
+        config_parser = subparsers.add_parser("config", help="Validate configuration")
         config_parser.set_defaults(func=self.config)
         config_parser.add_argument(
             "--check", help="Check config for any issues", action="store_true"
         )
+        backfill_parser = subparsers.add_parser("backfill", help="Backfill missing custom fields from available data")
+        backfill_parser.set_defaults(func=self.backfill)
+        backfill_parser.add_argument(
+            "--dry-run", help="Print changes instead of applying them", action="store_true"
+        )
+
+    def backfill(self):
+        engine = pyrosimple.connect()
+        engine.open()
+        matcher = matching.create_matcher('loaded=0 metafile=/.+/')
+        for i in engine.view('main', matching.create_matcher('loaded=0 metafile=/.+/')):
+            try:
+                mtime = int(Path(i.metafile).stat().st_mtime)
+                if self.args.dry_run:
+                    dt = datetime.fromtimestamp(mtime)
+                    print(f"Would set {i.hash} tm_loaded to {dt} from metafile {i.metafile}")
+                else:
+                    i.rpc_call("d.custom.set", ["tm_loaded", str(mtime)])
+                    i.flush()
+            except Exception as e:
+                print(f"Could not set tm_loaded for {i.hash}: {e}")
+        for i in engine.view('main', matching.create_matcher('loaded=0 path=/.+/')):
+            try:
+                mtime = int(Path(i.path).stat().st_mtime)
+                if self.args.dry_run:
+                    dt = datetime.fromtimestamp(mtime)
+                    print(f"Would set {i.hash} tm_loaded to {dt} from path {i.path}")
+                else:
+                    i.rpc_call("d.custom.set", ["tm_loaded", str(mtime)])
+                    i.flush()
+            except Exception as e:
+                print(f"Could not set tm_loaded for {i.hash}: {e}")
+        for i in engine.view('main', matching.create_matcher('completed=0 is_complete=yes path=/.+/')):
+            try:
+                mtime = int(Path(i.path).stat().st_mtime)
+                if self.args.dry_run:
+                    dt = datetime.fromtimestamp(mtime)
+                    print(f"Would set {i.hash} tm_completed to {dt} from path {i.path}")
+                else:
+                    i.rpc_call("d.custom.set", ["tm_completed", str(mtime)])
+                    i.flush()
+            except Exception as e:
+                print(f"Could not set tm_loaded for {i.hash}: {e}")
 
     def config(self):
         """Handle the config subcommand"""
