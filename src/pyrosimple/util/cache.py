1ccbb632fc530cfbb965d7f57e9bff22bbf4c4a1
kannibalox <kannibalox@gmail.com>
2022-09-12 23:36:02 -0400

    Add cache types

--- src/pyrosimple/util/cache.py
+++ src/pyrosimple/util/cache.py
@@ -3,40 +3,47 @@ import time
 
 from collections import abc
 from threading import RLock
-from typing import Dict, Optional
+from typing import Any, Dict, Hashable, Optional, Set, Tuple, Union
 
 
 class ExpiringCache(abc.MutableMapping):
     """Caches items for a fixed time, with an optional exlusionary list"""
 
-    def __init__(self, items: Optional[Dict] = None, expires=5, static_keys=None):
-        self.expires = expires
+    def __init__(
+        self,
+        items: Optional[Dict] = None,
+        expires: float = 5.0,
+        static_keys: Optional[Set] = None,
+    ):
+        self.expires: float = expires
         # When 3.7 support is dropped, the type can be made more specific:
         # Dict[str, tuple[float, Any]]
-        self.data: Dict[str, tuple] = {}
+        self.data: Dict[Hashable, tuple] = {}
         self.lock = RLock()
         self.static_keys = static_keys or set()
         if items:
             for key, val in items:
                 if key in self.static_keys:
-                    expire_at = 0
+                    expire_at = 0.0
                 else:
                     expire_at = self.expires + time.time()
                 self.data[key] = (expire_at, val)
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Hashable):
         with self.lock:
             del self.data[key]
 
-    def __setitem__(self, key, val, expire=None):
+    def __setitem__(self, key: Hashable, val: Any, expire: Optional[float] = None):
         with self.lock:
             if key in self.static_keys or expire == 0:
-                expire_at = 0
+                expire_at = 0.0
             else:
                 expire_at = (expire or self.expires) + time.time()
             self.data[key] = (expire_at, val)
 
-    def __getitem__(self, key, with_age=False):
+    def __getitem__(
+        self, key: Hashable, with_age: bool = False
+    ) -> Union[Tuple[Any, float], Any]:
         with self.lock:
             expires_at, item = self.data[key]
             if expires_at == 0 or expires_at > time.time():
@@ -46,12 +53,12 @@ class ExpiringCache(abc.MutableMapping):
             del self[key]
             raise KeyError(key)
 
-    def __contains__(self, key):
+    def __contains__(self, key: Hashable) -> bool:
         """Return True if the dict has a key, else return False."""
         try:
             with self.lock:
-                expires_at, _ = self.data.get(key)
-                if expires_at == 0 or expires_at < time.time():
+                expires_at, item = self.data.get(key, (0, None))
+                if item is not None and expires_at == 0 or expires_at < time.time():
                     return True
                 del self[key]
         except KeyError:
