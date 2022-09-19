9a650602cacf10bc2c4187ba0adab2110bfd8085
kannibalox <kannibalox@gmail.com>
2022-09-18 20:31:36 -0400

    Fix `views` required field

--- src/pyrosimple/torrent/engine.py
+++ src/pyrosimple/torrent/engine.py
@@ -316,7 +316,7 @@ def core_fields():
         "views this item is attached to",
         matcher=matching.TaggedAsFilter,
         formatter=_fmt_tags,
-        requires=["d.custom=tags"],
+        requires=["d.views"],
     )
     yield DynamicField(
         set,
