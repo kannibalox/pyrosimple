ec91243ea62763c5440d8b677d877fb99c778a2f
kannibalox <kannibalox@gmail.com>
2023-04-29 10:46:54 -0400

    Break tests in preparation for #33 fixing it

--- src/tests/test_pyroadmin.py
+++ src/tests/test_pyroadmin.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 
-from pyrosimple.config import settings
+from pyrosimple import config
 from pyrosimple.scripts.pyroadmin import AdminTool
 
 
 def test_pyroadmin_create_rc(tmp_path_factory):
     rc_path = Path(tmp_path_factory.mktemp("pyroadmin"), "rtorrent.rc")
-    settings.RTORRENT_RC = str(rc_path)
-    settings.CONFIG = "/dev/null"
+    config.settings.RTORRENT_RC = str(rc_path)
+    config.SETTINGS_FILE = "/dev/null"
     AdminTool().run(["config", "--create-rtorrent-rc"])
+    assert rc_path.stat().st_size >	0
 
 
 def test_pyroadmin_create_config(tmp_path_factory):
     config_path = Path(tmp_path_factory.mktemp("pyroadmin"), ".config", "config.toml")
-    settings.RTORRENT_RC = "/dev/null"
-    settings.CONFIG = str(config_path)
-    AdminTool().run(["config", "--create-config"])
+    config.settings.RTORRENT_RC = "/dev/null"
+    config.SETTINGS_FILE = str(config_path)
+    AdminTool().run(["--verbose", "config", "--create-config"])
+    assert config_path.stat().st_size > 0
