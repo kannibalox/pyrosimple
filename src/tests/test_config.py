f6c3b10e7c7c53b05d5eaa9a46f1736d39f37b66
kannibalox <kannibalox@gmail.com>
2023-05-20 10:02:50 -0400

    Add test case for parsing rtorrent.rc

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -3,16 +3,25 @@
 
     Copyright (c) 2011 The PyroScope Project <pyroscope.project@gmail.com>
 """
-import logging
-import unittest
+import os
+
+import pytest
 
 from pyrosimple import config
 
 
-class ConfigTest(unittest.TestCase):
-    def test_config(self):
-        pass
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize(
+    ("lines", "want"),
+    [
+        (
+            "network.scgi.open_local = ~/.config/rtorrent/socket",
+            "scgi+unix://" + os.path.expanduser("~/.config/rtorrent/socket"),
+        ),
+        ("scgi_local = /tmp/rtorrent.sock", "scgi+unix:///tmp/rtorrent.sock"),
+        ("scgi_local=/tmp/rtorrent.sock", "scgi+unix:///tmp/rtorrent.sock"),
+    ],
+)
+def test_rtorrentrc_parse(lines, want, tmpdir):
+    rc = tmpdir.join("rtorrrent.rc")
+    rc.write(lines)
+    assert config.scgi_url_from_rtorrentrc(rc) == want
