e3c280d20019f7471321795053b754c942e60b3e
kannibalox <kannibalox@gmail.com>
2023-07-08 09:01:04 -0400

    Formatting

--- src/pyrosimple/scripts/pyroui.py
+++ src/pyrosimple/scripts/pyroui.py
@@ -1,14 +1,29 @@
-import pyrosimple
-from pyrosimple.util import fmt
 from typing import Any
 
-from textual import work, on
+from textual import on, work
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer, Grid
+from textual.containers import Grid, ScrollableContainer
 from textual.reactive import reactive
+from textual.screen import ModalScreen, Screen
 from textual.validation import Function, Number, ValidationResult, Validator
-from textual.widgets import Header, Footer, DataTable, Static, TabbedContent, TabPane, Button, Input, Label, Tree, LoadingIndicator
-from textual.screen import Screen, ModalScreen
+from textual.widgets import (
+    Button,
+    DataTable,
+    Footer,
+    Header,
+    Input,
+    Label,
+    LoadingIndicator,
+    Static,
+    TabbedContent,
+    TabPane,
+    Tree,
+)
+
+import pyrosimple
+
+from pyrosimple.util import fmt
+
 
 class PeerTable(DataTable):
     def __init__(self, key):
@@ -17,7 +32,8 @@ class PeerTable(DataTable):
         self.cursor_type = "row"
 
     def on_mount(self):
-        self.add_columns("IP", "Up", "Down","Peer")
+        self.add_columns("IP", "Up", "Down", "Peer")
+
 
 class TrackerTable(DataTable):
     def __init__(self, key):
@@ -28,6 +44,7 @@ class TrackerTable(DataTable):
     def on_mount(self):
         self.add_columns("Tracker")
 
+
 class FilterValidator(Validator):
     def validate(self, value: str) -> ValidationResult:
         """Check a string is equal to its reverse."""
@@ -37,10 +54,15 @@ class FilterValidator(Validator):
             return self.failure(str(e))
         return self.success()
 
+
 class FilterEdit(ModalScreen):
     def compose(self):
         yield Grid(
-            Input(value=self.app.rtorrent_filter, id="filter_input", validators=[FilterValidator()]),
+            Input(
+                value=self.app.rtorrent_filter,
+                id="filter_input",
+                validators=[FilterValidator()],
+            ),
             Button("Apply", variant="primary", id="apply"),
             Button("Cancel", variant="error", id="cancel"),
             id="filter_dialog",
@@ -62,10 +84,10 @@ class FilterEdit(ModalScreen):
             self.app.load_data()
         self.dismiss(False)
 
+
 class TorrentInfoScreen(ModalScreen):
-    BINDINGS = [
-        ("escape", "escape", "Return to main screen")
-    ]
+    BINDINGS = [("escape", "escape", "Return to main screen")]
+
     def __init__(self, key):
         self.key = key
         self.info_template = """
@@ -106,54 +128,82 @@ class TorrentInfoScreen(ModalScreen):
         engine.open()
         text = self.query_one("#static_info")
         prefetch = []
-        for p in pyrosimple.torrent.rtorrent.get_fields_from_template(self.info_template):
+        for p in pyrosimple.torrent.rtorrent.get_fields_from_template(
+            self.info_template
+        ):
             prefetch.extend(pyrosimple.torrent.engine.field_lookup(p).requires)
         view = engine.view(self.key.value)
         template = pyrosimple.torrent.rtorrent.env.from_string(self.info_template)
         item = list(engine.items(view, prefetch=prefetch))[0]
         text.update(pyrosimple.torrent.rtorrent.format_item(template, item))
-        
+
     @on(TabbedContent.TabActivated, "#torrent_info_tabs", tab="#peers")
     @work(exclusive=True)
     def load_peer_data(self) -> None:
         fields = []
         proxy = self.app.rtorrent_engine.open()
         table = self.query_one(PeerTable)
-        fields = ["address","up_rate", "down_rate", "client_version"]
-        result = proxy.p.multicall(self.key.value, '', *[f"p.{f}=" for f in fields])
+        fields = ["address", "up_rate", "down_rate", "client_version"]
+        result = proxy.p.multicall(self.key.value, "", *[f"p.{f}=" for f in fields])
         table.clear()
         for peer in result:
             data = dict(zip(fields, peer))
             if int(data["up_rate"]) != 0:
-                up = fmt.human_size(data["up_rate"])+"/s"
+                up = fmt.human_size(data["up_rate"]) + "/s"
             table.add_row(
                 data["address"],
-                fmt.human_size(data["up_rate"])+"/s" if int(data["up_rate"]) !=	0 else "-",
-                fmt.human_size(data["down_rate"])+"/s" if int(data["down_rate"]) != 0 else "-",
-                data["client_version"]
+                fmt.human_size(data["up_rate"]) + "/s"
+                if int(data["up_rate"]) != 0
+                else "-",
+                fmt.human_size(data["down_rate"]) + "/s"
+                if int(data["down_rate"]) != 0
+                else "-",
+                data["client_version"],
             )
-        
+
     def on_mount(self):
         self.load_info_data()
 
     def action_escape(self):
         self.app.pop_screen()
-        
+
+
 class CollapsedTorrentTable(DataTable):
     COLUMNS = [
-        ("❢", {"key": "error", "width": 1}, "{% if d.message %}[bold red reverse]!{%endif%}"),
-        ("☢", {"key": "state", "width": 1}, "{% if d.is_open and d.is_active %}▹{% else %}▪{%endif%}"),
+        (
+            "❢",
+            {"key": "error", "width": 1},
+            "{% if d.message %}[bold red reverse]!{%endif%}",
+        ),
+        (
+            "☢",
+            {"key": "state", "width": 1},
+            "{% if d.is_open and d.is_active %}▹{% else %}▪{%endif%}",
+        ),
         ("☍ ", {"key": "tied", "width": 1}, "{% if d.metafile %}⚯{%endif%}"),
         ("⌘", {"key": "ignored"}, "{% if d.is_ignored %}⚒{%else%}◌{%endif%}"),
-        ("☇", {"key": "xfer", "width": 1}, "{% if d.up and d.down%}⇅{%elif d.up %}↟{%elif d.down %}↡{%endif%}"),
+        (
+            "☇",
+            {"key": "xfer", "width": 1},
+            "{% if d.up and d.down%}⇅{%elif d.up %}↟{%elif d.down %}↡{%endif%}",
+        ),
         ("↡", {"key": "down"}, "{% if d.down != 0%}{{d.down|sz}}/s{%endif%}"),
         ("↟", {"key": "up"}, "{% if d.up != 0%}{{d.up|sz}}/s{%endif%}"),
         ("℞", {"key": "peers_connected"}, "{{d.d_peers_connected}}"),
-        ("⣿", {"key": "progress", "width": 1}, "{% if d.is_complete %}❚{%elif d.done == 0%} {%else%}{{ '⠁⠉⠋⠛⠟⠿⡿⣿'[d.done|int//8]}}{%endif%}"),
+        (
+            "⣿",
+            {"key": "progress", "width": 1},
+            "{% if d.is_complete %}❚{%elif d.done == 0%} {%else%}{{ '⠁⠉⠋⠛⠟⠿⡿⣿'[d.done|int//8]}}{%endif%}",
+        ),
         ("⛁", {"key": "size"}, "{{d.size|sz}}"),
         ("T", {"key": "alias"}, "{{d.alias}}"),
-        ("Name", {"key": "name"}, "{%set rich='magenta'%}{%if d.is_complete%}{%set rich='green'%}{%endif%}[{{rich}}]{{d.name}}"),
+        (
+            "Name",
+            {"key": "name"},
+            "{%set rich='magenta'%}{%if d.is_complete%}{%set rich='green'%}{%endif%}[{{rich}}]{{d.name}}",
+        ),
     ]
+
     def __init__(self, engine) -> None:
         self.rtorrent_engine = engine
         super().__init__(zebra_stripes=True)
@@ -170,8 +220,10 @@ class CollapsedTorrentTable(DataTable):
     def on_data_table_row_selected(self, event) -> None:
         self.app.push_screen(TorrentInfoScreen(event.row_key))
 
+
 class MainScreen(Screen):
     pass
+
 
 class DownloadHeader(Static):
     view = reactive("main")
@@ -216,7 +268,9 @@ class PyroUIApp(App):
     def load_engine(self) -> None:
         self.rtorrent_engine.open()
         props = self.rtorrent_engine.properties
-        self.query_one("#rtorrent_info").renderable = f"*** rTorrent {props['system.client_version']}/{props['system.library_version']} - {props['session.name']} ***"
+        self.query_one(
+            "#rtorrent_info"
+        ).renderable = f"*** rTorrent {props['system.client_version']}/{props['system.library_version']} - {props['session.name']} ***"
         header = self.query_one("#pyroui_header")
         header.view = self.rtorrent_view
         header.filter = self.rtorrent_filter
@@ -237,7 +291,9 @@ class PyroUIApp(App):
         sort_key = pyrosimple.torrent.rtorrent.validate_sort_fields(self.rtorrent_sort)
         items.sort(key=sort_key)
         header = self.query_one("#pyroui_header")
-        is_refresh = (self.rtorrent_filter == header.filter) and (self.rtorrent_view == header.view)
+        is_refresh = (self.rtorrent_filter == header.filter) and (
+            self.rtorrent_view == header.view
+        )
         # This shouldn't change wildly
         if is_refresh:
             hashes = list([i.hash for i in items])
@@ -247,7 +303,9 @@ class PyroUIApp(App):
                 if row.value not in hashes:
                     table.remove_row(row)
             for item in items:
-                data = list(pyrosimple.torrent.rtorrent.format_item(template, item).split("\t"))
+                data = list(
+                    pyrosimple.torrent.rtorrent.format_item(template, item).split("\t")
+                )
                 # Create
                 if item.hash not in table_hashes:
                     table.add_row(*data, key=item.hash)
@@ -259,7 +317,9 @@ class PyroUIApp(App):
             table.disabled = True
             table.clear()
             for item in items:
-                data = pyrosimple.torrent.rtorrent.format_item(template, item).split("\t")
+                data = pyrosimple.torrent.rtorrent.format_item(template, item).split(
+                    "\t"
+                )
                 table.add_row(*data, key=item.hash)
             header.view = self.rtorrent_view
             header.filter = self.rtorrent_filter
@@ -276,22 +336,23 @@ class PyroUIApp(App):
 
     def action_refresh(self) -> None:
         self.load_data()
-        
+
     def action_edit_filter(self) -> None:
         self.push_screen(FilterEdit())
-        
+
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
-        
 
 def run():
     import sys
+
     app = PyroUIApp()
     if sys.argv[1]:
         app.rtorrent_filter = sys.argv[1]
     app.run()
+
 
 if __name__ == "__main__":
     run()
