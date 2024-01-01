d2165a2026791d694886800e8c3360aad0e936cc
kannibalox <kannibalox@gmail.com>
2024-01-01 12:47:02 -0500

    Add test for making magnet URIs

--- src/tests/test_mktor.py
+++ src/tests/test_mktor.py
@@ -5,6 +5,7 @@ import pytest
 from pyrosimple.scripts.base import ScriptBase
 from pyrosimple.scripts.lstor import MetafileLister
 from pyrosimple.scripts.mktor import MetafileCreator
+from pyrosimple.util.metafile import Metafile
 
 
 def test_mktor(tmp_path_factory):
@@ -13,6 +14,20 @@ def test_mktor(tmp_path_factory):
         fh.write("Hello world!")
     MetafileCreator().run([str(test_file), "http://example.com"])
     MetafileLister().run([str(test_file.with_suffix(".torrent"))])
+
+
+def test_mktor_magnet(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("mktor")
+    magnet_string = (
+        "magnet:?xt=urn:btih:1447bb03de993e1ee7e430526ff1fbac0daf7b44&dn=hello.txt"
+    )
+    MetafileCreator().run([magnet_string, "--magnet-watch", str(tmp_path)])
+    Metafile.from_file(
+        Path(
+            tmp_path,
+            "magnet-hello.txt-1447bb03de993e1ee7e430526ff1fbac0daf7b44.torrent",
+        )
+    )["magnet-uri"] == magnet_string
 
 
 def test_mktor_output(tmp_path_factory):
