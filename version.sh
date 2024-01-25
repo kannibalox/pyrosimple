afe095d9b0c676cc6aa270d2df866dae6d360f2e
kannibalox <kannibalox@gmail.com>
2024-01-25 08:54:32 -0500

    Fix markdown syntax in version script

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ new_version="$(poetry version -s --no-ansi)"
 
 sed -i -e "/## \[Unreleased\]/a \\\n## [${new_version}] - $(date +%Y-%m-%d)" \
     -e "s|\[Unreleased\]: .*|[Unreleased]: https://github.com/kannibalox/pyrosimple/compare/v${new_version}...HEAD|" \
-    -e "/^\[Unreleased\]/a [${new_version}] https://github.com/kannibalox/pyrosimple/compare/v${prev_version}...v${new_version}" \
+    -e "/^\[Unreleased\]/a [${new_version}]: https://github.com/kannibalox/pyrosimple/compare/v${prev_version}...v${new_version}" \
     CHANGELOG.md
 awk "/## \\[${new_version}\\]/,/## \\[${prev_version}\\]/" CHANGELOG.md | sed -e 1,2d | head -n -2 > RELEASE_NOTE.md
 poetry build
